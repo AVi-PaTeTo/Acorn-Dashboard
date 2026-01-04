3556b1bdfea47b908158c258637d2c0fce069f13
MushyKeycaps <abhi.soy.332@gmail.com>
2026-01-04 22:43:31 +0530

    minor fix

--- acorn_dashboard/src/modules/DetailedIndividualInsights.jsx
+++ acorn_dashboard/src/modules/DetailedIndividualInsights.jsx
@@ -43,7 +43,7 @@ export default function DetailedIndividualInsights(){
                         <div className='grid grid-cols-2 grid-rows-4 h-full gap-1'>
 
                             {/* Card #2 */}
-                            <span className='col-start-1 col-end-2 row-start-1 xl:row-end-4 2xl:row-end-4 bg-white rounded-xl flex flex-col p-2 pb-1'>
+                            <span className='col-start-1 col-end-2 row-start-1 row-end-4 bg-white rounded-xl flex flex-col p-2 pb-1'>
                                 <span className='flex gap-1 z-10 items-center text-[.9rem]'>
                                     <svg className='w-[22px] h-[22px] shrink-0 fill-pink-600' viewBox="0 0 24 24"><path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg>
                                     Instagram
